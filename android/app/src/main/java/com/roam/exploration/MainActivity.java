2e51911c1bfb0c24caa2d9a9cabeb143cfc5ec45
Tim Plummer <timpl12@gmail.com>
2026-09-10 19:40:59 +0200

    Sync district hover interactions

--- android/app/src/main/java/com/roam/exploration/MainActivity.java
+++ android/app/src/main/java/com/roam/exploration/MainActivity.java
@@ -10,10 +10,15 @@ public class MainActivity extends BridgeActivity {
     @Override public void onCreate(Bundle state) {
         registerPlugin(RideTrackingPlugin.class);
         super.onCreate(state);
-        getWindow().setStatusBarColor(Color.rgb(13, 16, 17));
-        getWindow().setNavigationBarColor(Color.rgb(13, 16, 17));
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             getWindow().setNavigationBarDividerColor(Color.rgb(13, 16, 17));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setStatusBarContrastEnforced(false);
+            getWindow().setNavigationBarContrastEnforced(false);
         }
         WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightStatusBars(false);
         WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightNavigationBars(false);
